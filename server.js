1096f1cd7f4cad07f1d53fea77f93531dffbcd8c
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2015-10-28 16:00:45 -0700

    Add PUT todos/:id

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/todos/:id', function (req, res){
 
 // POST /todos
 app.post('/todos', function (req, res) {
-	body = _.pick(req.body, 'description', 'completed');
+	var body = _.pick(req.body, 'description', 'completed');
 
 	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
 		return res.status(400).send();
@@ -60,9 +60,36 @@ app.delete('/todos/:id', function (req, res) {
 	
 });
 
-// var todoId = parseInt(req.params.id, 10);
-// 	var found = _.findWhere(todos, {id: todoId});
-// _.without
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var found = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!found){
+		return res.status(404).send();
+	}
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	} 
+
+	//length greater than zero and string and exists
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description=body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	_.extend(found, validAttributes);
+	res.json(found);
+	
+
+
+});
 
 app.listen(PORT, function(){
 	console.log("Express listening on port " + PORT);
