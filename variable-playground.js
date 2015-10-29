1a603169050b22c1d005ee792a91edfe8f4f0c37
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2015-10-28 17:19:54 -0700

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+// 	name: "Nick",
+// 	age: 22
+// };
+
+// function updatePerson (obj) {
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array Example
+
+var grades = [15, 37];
+
+function addGrade(gradesArr){
+	gradesArr.push(22);
+	debugger;
+}
+
+
+addGrade(grades);
+console.log(grades);
+
