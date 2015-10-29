fd6e029c122430c634a419bd8f33e812293e7263
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2015-10-29 16:21:45 -0700

    get todos/id to use sequelize

--- server.js
+++ server.js
@@ -43,15 +43,16 @@ app.get('/todos', function(req, res) {
 
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var found = _.findWhere(todos, {
-		id: todoId
-	});
 
-	if (found) {
-		res.json(found);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(todoId).then(function(todo){
+		if (!! todo){
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function (e) {
+		res.status(500).send();
+	})
 
 });
 
@@ -59,26 +60,12 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-
-	// call db.todo.create
-	//	respond with 200 and todo use toJSON()
-	// if it fails e res.status(400).json(e)
 	db.todo.create(body).then(function (todo){
 		res.json(todo.toJSON());
-	}).catch(function (e) {
+	}, function (e) {
 		res.status(400).json(e);
 	});
-
-
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).send();
-	// }
-	// body.description = body.description.trim();
-	// body.id = todoNextId++;
-
-	// todos.push(body);
-
-	// res.json(body);
+	
 });
 
 // DELETE /todos/:id
