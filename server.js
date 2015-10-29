a0f4bf36b859ca1a719fda2804cf82cb4fcda167
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2015-10-29 15:38:16 -0700

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -58,15 +59,26 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
-	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-		return res.status(400).send();
-	}
-	body.description = body.description.trim();
-	body.id = todoNextId++;
 
-	todos.push(body);
+	// call db.todo.create
+	//	respond with 200 and todo use toJSON()
+	// if it fails e res.status(400).json(e)
+	db.todo.create(body).then(function (todo){
+		res.json(todo.toJSON());
+	}).catch(function (e) {
+		res.status(400).json(e);
+	});
 
-	res.json(body);
+
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
+	// body.description = body.description.trim();
+	// body.id = todoNextId++;
+
+	// todos.push(body);
+
+	// res.json(body);
 });
 
 // DELETE /todos/:id
@@ -119,6 +131,12 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
-app.listen(PORT, function() {
-	console.log("Express listening on port " + PORT);
-})
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log("Express listening on port " + PORT);
+	});
+});
+
+// app.listen(PORT, function() {
+// 	console.log("Express listening on port " + PORT);
+// })
